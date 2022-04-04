88f452a189b632c5ebfd42b3e7f361d2a73fcda8
Rakhat <95361982+latest-update@users.noreply.github.com>
2022-04-04 17:50:13 +0600

    d2

--- config/database.php
+++ config/database.php
@@ -74,7 +74,7 @@ return [
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
-            'search_path' => 'public',
+            'd2odo883klso7o' => 'public',
             'sslmode' => 'prefer',
         ],
 
